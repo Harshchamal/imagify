fcbde7bde56cd669f9ba2d494a21b47175b6922d
ChamalHarsha <162911375+Harshchamal@users.noreply.github.com>
2025-06-21 18:34:43 +0530

    Fix .env format and update server config

--- Backend/server.js
+++ Backend/server.js
@@ -9,25 +9,15 @@ import imageRouter from './routes/imageRoutes.js'
 const PORT = process.env.PORT || 4000
 const app = express()
 
-// ✅ CORS configuration
-const allowedOrigins = [
-  'https://imagify-otji.vercel.app', // your frontend Vercel link
-  'http://localhost:5173' // for local testing
-]
-
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}))
 
 app.use(express.json())
-
+app.use(cors())
 await connectDB()
+
 
 app.use('/api/user', userRouter)
 app.use('/api/image', imageRouter)
+app.get('/', (req, res)=> res.send("API Working "))
 
-app.get('/', (req, res) => res.send("API Working"))
 
-app.listen(PORT, () => console.log('Server running on port ' + PORT))
+app.listen(PORT, ()=> console.log('Server runing on port'+ PORT));
